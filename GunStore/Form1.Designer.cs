271e3e6e407d5f19daf0c78d81b5d5ba4674cf47
svosin <svosin@airmail.cc>
2020-06-13 11:41:25 +0300

    starting to work on UI

--- GunStore/Form1.Designer.cs
+++ GunStore/Form1.Designer.cs
@@ -28,13 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.показатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.заказыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.клиентыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.гладкоствольноеОружиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.нарезноеОружиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оООПToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(828, 544);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(820, 518);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(820, 542);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.показатьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(828, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // показатьToolStripMenuItem
+            // 
+            this.показатьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.заказыToolStripMenuItem,
+            this.клиентыToolStripMenuItem,
+            this.гладкоствольноеОружиеToolStripMenuItem,
+            this.нарезноеОружиеToolStripMenuItem,
+            this.оООПToolStripMenuItem});
+            this.показатьToolStripMenuItem.Name = "показатьToolStripMenuItem";
+            this.показатьToolStripMenuItem.Size = new System.Drawing.Size(78, 20);
+            this.показатьToolStripMenuItem.Text = "Показать...";
+            // 
+            // заказыToolStripMenuItem
+            // 
+            this.заказыToolStripMenuItem.Name = "заказыToolStripMenuItem";
+            this.заказыToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.заказыToolStripMenuItem.Text = "Заказы";
+            // 
+            // клиентыToolStripMenuItem
+            // 
+            this.клиентыToolStripMenuItem.Name = "клиентыToolStripMenuItem";
+            this.клиентыToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.клиентыToolStripMenuItem.Text = "Клиенты";
+            // 
+            // гладкоствольноеОружиеToolStripMenuItem
+            // 
+            this.гладкоствольноеОружиеToolStripMenuItem.Name = "гладкоствольноеОружиеToolStripMenuItem";
+            this.гладкоствольноеОружиеToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.гладкоствольноеОружиеToolStripMenuItem.Text = "Гладкоствольное оружие";
+            // 
+            // нарезноеОружиеToolStripMenuItem
+            // 
+            this.нарезноеОружиеToolStripMenuItem.Name = "нарезноеОружиеToolStripMenuItem";
+            this.нарезноеОружиеToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.нарезноеОружиеToolStripMenuItem.Text = "Нарезное оружие";
+            // 
+            // оООПToolStripMenuItem
+            // 
+            this.оООПToolStripMenuItem.Name = "оООПToolStripMenuItem";
+            this.оООПToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.оООПToolStripMenuItem.Text = "ОООП";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(828, 568);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem показатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem заказыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem клиентыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem гладкоствольноеОружиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem нарезноеОружиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оООПToolStripMenuItem;
     }
 }
 
--- GunStore/OrderListUc.cs
+++ GunStore/OrderListUc.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +10,13 @@ using System.Windows.Forms;
 
 namespace GunStore
 {
-    public partial class Form1 : Form
+    public partial class OrderListUc : UserControl
     {
-        public Form1()
+        public OrderListUc()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            tabControl1.TabPages[0].Controls.Add(new OrderListUc());
-        }
+        
     }
 }
